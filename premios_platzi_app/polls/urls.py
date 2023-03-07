4abd948a6d9076e5c0b2e39cbb2dc3352a609c37
ecst1999 <javier_zam1998@live.com>
2023-03-07 08:54:29 -0500

    Complete

--- premios_platzi_app/polls/urls.py
+++ premios_platzi_app/polls/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 app_name = "polls"
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     # example: /polls/
-    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     # example: /polls/5/results
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:pk>/results/', views.ResultView.as_view(), name='results'),
     # example: /polls/5/vote
     path('<int:question_id>/vote/', views.vote, name='vote'),    
 ]
